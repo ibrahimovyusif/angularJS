444ec5ffc883a5a81916a5a63e0e1fc07dd38d0d
ibrahimovyusif <yusifibrahimov@hotmail.com>
2015-10-20 18:04:52 +0500

    s

---
+++ app.js
@@ -0,0 +1,13 @@
+// MODULE
+var angularApp = angular.module('angularApp', []);
+
+// CONTROLLERS
+angularApp.controller('mainController', ['$scope','$filter', function ($scope, $filter) {
+    $scope.handle = '';
+    $scope.lowercasehandle = function () {
+      return $filter('lowercase')($scope.handle);                                   
+    };
+}]);
+
+
+
